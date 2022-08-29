017e40413dd83afc83629c61e75133b49f4e0a3c
Andrew <andrew.painter767@gmail.com>
2022-08-29 16:51:26 -0400

    intelij file

---
+++ Final group project/aws-blogs/src/main/java/com/group/awsblogs/AwsBlogsApplication.java
@@ -0,0 +1,13 @@
+package com.group.awsblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBlogsApplication.class, args);
+	}
+
+}
