9e12242f205c30f98e201c76ebaa0881ca66f5f2
Andrew <andrew.painter767@gmail.com>
2022-09-02 11:56:53 -0400

    navbar

--- Final group project/aws-blog-upload/src/main/frontend/src/App.js
+++ Final group project/aws-blog-upload/src/main/frontend/src/App.js
@@ -1,80 +1,24 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import './App.css';
-import axios from 'axios';
-import Dropzone, {useDropzone} from 'react-dropzone'
-
-
-const UserProfiles = () => {
-
-  const [userProfiles, setUserProfiles] = useState([]);
-
-  const fetchUserProfiles = () => {
-    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
-      console.log(res);
-      setUserProfiles(res.data)
-    })
-  }
-
-  useEffect(()=>{
-    fetchUserProfiles();
-  }, []);
-  return userProfiles.map((userProfile, index)=>{
-    return(
-      <div key={index}>
-        {/*todo: profile image*/}
-        <br/>
-        <br/>
-        <h1>{userProfile.username}</h1>
-        <p>{userProfile.userProfileId}</p>
-        <MyDropzone  {...userProfile}/>
-        <br/>
-      </div>
-    )
-  })
-};
-
-function MyDropzone({ userProfileId }) {
-  const onDrop = useCallback(acceptedFiles => {
-    const file = acceptedFiles[0];
-    console.log(file);
-
-    const formData = new FormData();
-    formData.append("file",file);
-
-    axios.post('http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload',
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data"
-      }
-    }
-    ).then(()=> {
-      console.log("file uploaded")
-    }).catch(err => {
-      console.log(err);
-    });
-  }, []);
-
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the image here ...</p> :
-          <p>Drag 'n' drop profile image, or click to select image</p>
-      }
-    </div>
-  )
-}
+import Navbar from './compentents/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
 
 
 function App() {
   return (
-    <div className="App">
-      <UserProfiles/>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact component = {Home}/>
+        <Route path='/reports' component = {Reports}/>
+        <Route path='/products' component = {Products}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
